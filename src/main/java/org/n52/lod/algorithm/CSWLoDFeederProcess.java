c5e37fbc96387f12fa2cdbb0bd1b8467ff79a440
Daniel Nüst <d.nuest@52north.org>
2014-08-27 13:52:20 +0200

    remove own utils class, use Guava instead, add empty WPS process

---
+++ src/main/java/org/n52/lod/algorithm/CSWLoDFeederProcess.java
@@ -0,0 +1,9 @@
+package org.n52.lod.algorithm;
+
+import org.n52.wps.algorithm.annotation.Algorithm;
+
+
+@Algorithm(version = "0.1")
+public class CSWLoDFeederProcess {
+    
+}
--- src/test/java/org/n52/lod/csw/CatalogInteractorTestManual.java
+++ src/test/java/org/n52/lod/csw/CatalogInteractorTestManual.java
@@ -4,38 +4,54 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.apache.xmlbeans.XmlException;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.n52.util.IoUtils;
+import org.n52.oxf.util.web.HttpClientException;
 
 public class CatalogInteractorTestManual {
-    
+
     private static Path tempdir;
 
     @BeforeClass
     public static void tempdir() throws IOException {
-         tempdir = Files.createTempDirectory("csw2lod_");
+        tempdir = Files.createTempDirectory("csw2lod_");
     }
-    
+
+    private CatalogInteractor interactor;
+
+    @Before
+    public void createInteractor() throws IOException {
+        interactor = new CatalogInteractor();
+    }
+
     @Test
     public void testExecuteGetRecords() throws Exception {
-        String result = new CatalogInteractor().executeGetRecords(10, 2000);
-        
+        String result = interactor.executeGetRecords(10, 2000);
+
         Path file = Files.createFile(tempdir.resolve("catalogResult_GetRecords.xml"));
-        
-        IoUtils.saveFile(file.toFile(), result);
+
+        Files.write(file, result.getBytes());
 
         System.out.println("Saved file to " + file);
     }
 
     @Test
     public void testExecuteGetRecordsById() throws IOException, Exception {
-        String result = new CatalogInteractor().executeGetRecordsById(Constants.getInstance().getTestRecordId());
+        String result = interactor.executeGetRecordsById(Constants.getInstance().getTestRecordId());
 
         Path file = Files.createFile(tempdir.resolve("catalogResult_GetRecordsById.xml"));
-        IoUtils.saveFile(file.toFile(), result);
+        Files.write(file, result.getBytes());
 
         System.out.println("Saved file to " + file);
+    }
+
+    @Test
+    public void countRecordsInCSW() throws IllegalStateException, HttpClientException, IOException, XmlException {
+        long numberOfRecords = interactor.getNumberOfRecords();
+
+        System.out.println(numberOfRecords);
     }
 
 }
